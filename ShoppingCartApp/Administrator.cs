ab3fd290d3d16fd0acc3769feb1620d81e8153a9
Hemory Phifer <hemphife@gmail.com>
2021-01-13 16:49:56 -0500

    Creating an admin flow

---
+++ ShoppingCartApp/Administrator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCartApp
+{
+   public class Administrator
+    {
+        public string UserName { get; set; }
+
+        public int Pin { get; set; }
+
+        public Administrator(string userName, int pin)
+        {
+            UserName = userName;
+            Pin = pin;
+        }
+
+    }
+}
