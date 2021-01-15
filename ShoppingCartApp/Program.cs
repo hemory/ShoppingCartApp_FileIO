fa3f2dab009aeccf20e4245da4469866c603a1b4
Hemory Phifer <hemphife@gmail.com>
2021-01-15 09:24:59 -0500

    added validation

--- ShoppingCartApp/Program.cs
+++ ShoppingCartApp/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Sockets;
 using System.Threading;
 
 namespace ShoppingCartApp
@@ -18,15 +17,17 @@ namespace ShoppingCartApp
             bool continueToRegularApp = true;
 
 
-
+            //Admins List
             List<Administrator> admins = new List<Administrator>()
             {
                 new Administrator("hem", 1234)
             };
 
-            Console.WriteLine("Enter 'admin' to access admin menu or press any key to continue");
-            string accessAdminMenu = Console.ReadLine().ToLower().Trim();
+            Console.Write("Enter 'admin' to access admin menu or press any key to continue: ");
 
+
+
+            string accessAdminMenu = Console.ReadLine().ToLower().Trim();
 
             if (accessAdminMenu == "admin")
             {
@@ -37,9 +38,21 @@ namespace ShoppingCartApp
                     Console.Write("User Name: ");
                     string userName = Console.ReadLine();
 
+                    while (string.IsNullOrEmpty(userName))
+                    {
+                        Console.Write("Try again: ");
+                        userName = Console.ReadLine().ToLower().Trim();
+                    }
+
 
                     Console.Write("Pin: ");
-                    int pin = int.Parse(Console.ReadLine());
+
+                    bool isInt = int.TryParse(Console.ReadLine(), out int pin);
+                    while (isInt == false)
+                    {
+                        Console.Write("Pin: ");
+                        isInt = int.TryParse(Console.ReadLine(), out pin);
+                    }
 
                     Thread.Sleep(2000);
                     Console.Clear();
@@ -50,11 +63,17 @@ namespace ShoppingCartApp
                         {
                             isAdmin = true;
                             bool keepGoing = true;
-                            //todo admin menu
+
                             while (keepGoing)
                             {
                                 Console.WriteLine("[1]Add Product [2]Update Product Inventory [3]Delete Product");
                                 string adminChoice = Console.ReadLine().Trim();
+
+                                while (string.IsNullOrEmpty(adminChoice))
+                                {
+                                    Console.Write("Try again: ");
+                                    adminChoice = Console.ReadLine().ToLower().Trim();
+                                }
 
                                 Thread.Sleep(1000);
                                 Console.Clear();
@@ -70,17 +89,60 @@ namespace ShoppingCartApp
                                         Console.Write("Name: ");
                                         product.Name = Console.ReadLine().ToLower().Trim();
 
+                                        while (string.IsNullOrEmpty(product.Name))
+                                        {
+                                            Console.Write("Try again: ");
+                                            product.Name = Console.ReadLine().ToLower().Trim();
+                                        }
+
+
                                         Console.Write("Description: ");
                                         product.Description = Console.ReadLine().ToLower().Trim();
+
+                                        while (string.IsNullOrEmpty(product.Description))
+                                        {
+                                            Console.Write("Try again: ");
+                                            product.Description = Console.ReadLine().ToLower().Trim();
+                                        }
+
 
                                         Console.Write("Category: ");
                                         product.Category = Console.ReadLine().ToLower().Trim();
 
+                                        while (string.IsNullOrEmpty(product.Category))
+                                        {
+                                            Console.Write("Try again: ");
+                                            product.Category = Console.ReadLine().ToLower().Trim();
+                                        }
+
+
+
                                         Console.Write("Price: ");
-                                        product.Price = double.Parse(Console.ReadLine().Trim());
+                                        isInt = int.TryParse(Console.ReadLine(), out int price);
+                                        while (isInt == false)
+                                        {
+                                            Console.Write("Price: ");
+                                            isInt = int.TryParse(Console.ReadLine(), out price);
+                                        }
+
+                                        product.Price = price;
+
+
+
+
 
                                         Console.Write("Inventory: ");
-                                        product.Inventory = int.Parse(Console.ReadLine().Trim());
+
+                                        isInt = int.TryParse(Console.ReadLine(), out int inventory);
+                                        while (isInt == false)
+                                        {
+                                            Console.Write("Inventory: ");
+                                            isInt = int.TryParse(Console.ReadLine(), out inventory);
+                                        }
+
+                                        product.Inventory = inventory;
+
+
 
                                         productsFromRepo.Products.Add(product);
 
@@ -98,8 +160,18 @@ namespace ShoppingCartApp
                                         keepGoing = false;
 
                                         DisplayProductsForAdmin(productsFromRepo);
+
+
                                         Console.Write("Please choose one to update: ");
-                                        int idToUpdate = int.Parse(Console.ReadLine());
+
+                                        isInt = int.TryParse(Console.ReadLine(), out int idToUpdate);
+                                        while (isInt == false)
+                                        {
+                                            Console.Write("Please choose one to update: ");
+                                            isInt = int.TryParse(Console.ReadLine(), out idToUpdate);
+                                        }
+
+
 
                                         Thread.Sleep(2000);
                                         Console.Clear();
@@ -107,12 +179,23 @@ namespace ShoppingCartApp
                                         var productToUpdate = productsFromRepo.Products.Find(p => p.Id == idToUpdate);
 
                                         productsFromRepo.Products.Remove(productToUpdate);
-                                        
+
                                         Console.WriteLine($"The current inventory is {productToUpdate.Inventory}.");
                                         Console.Write("How many would you like to add to the inventory: ");
-                                        productToUpdate.Inventory += int.Parse(Console.ReadLine());
 
-                                       productsFromRepo.Products.Add(productToUpdate);
+                                        isInt = int.TryParse(Console.ReadLine(), out int newInventory);
+                                        while (isInt == false)
+                                        {
+                                            Console.Write("How many would you like to add to the inventory: ");
+                                            isInt = int.TryParse(Console.ReadLine(), out newInventory);
+                                        }
+
+                                        productToUpdate.Inventory += newInventory;
+
+
+
+
+                                        productsFromRepo.Products.Add(productToUpdate);
 
                                         Console.WriteLine("Here is your updated list.");
 
@@ -128,7 +211,14 @@ namespace ShoppingCartApp
                                         DisplayProducts(productsFromRepo);
                                         Console.WriteLine();
                                         Console.Write("Please choose one to delete: ");
-                                        int idToDelete = int.Parse(Console.ReadLine());
+
+                                        isInt = int.TryParse(Console.ReadLine(), out int idToDelete);
+                                        while (isInt == false)
+                                        {
+                                            Console.Write("Please choose one to delete: ");
+                                            isInt = int.TryParse(Console.ReadLine(), out idToDelete);
+                                        }
+
 
                                         Thread.Sleep(2000);
                                         Console.Clear();
@@ -158,8 +248,9 @@ namespace ShoppingCartApp
                                 Thread.Sleep(2000);
                                 Console.Clear();
 
-                                Console.WriteLine("Return to Admin Menu? Enter [y] to return or any key to exit application.");
+                                Console.Write("Return to Admin Menu? Enter [y] to return or any key to exit application: ");
                                 string choiceToReturnToAdminMenu = Console.ReadLine().ToLower().Trim();
+
 
                                 if (choiceToReturnToAdminMenu == "y")
                                 {
@@ -171,7 +262,7 @@ namespace ShoppingCartApp
                         {
                             if (i <= 3)
                             {
-                                Console.WriteLine("Invalid user name or pin. Try again."); 
+                                Console.WriteLine("Invalid user name or pin. Try again.");
                             }
                             break;
                         }
@@ -180,21 +271,22 @@ namespace ShoppingCartApp
 
                     if (isAdmin)
                     {
-                      break;
+                        break;
                     }
                 }
 
                 Console.WriteLine();
                 if (isAdmin == false)
                 {
-                    Console.WriteLine("Sorry, we cannot verify you as an admin."); 
+                    Console.WriteLine("Sorry, we cannot verify you as an admin.");
                 }
 
                 Thread.Sleep(2000);
                 Console.Clear();
 
-                Console.WriteLine("Continue to regular app? Enter [n] to exit or any key to continue to regular app.");
+                Console.Write("Continue to regular app? Enter [n] to exit or any key to continue to regular app: ");
                 string response = Console.ReadLine().ToLower().Trim();
+
 
                 if (response == "n")
                 {
@@ -203,7 +295,7 @@ namespace ShoppingCartApp
 
                 Thread.Sleep(2000);
                 Console.Clear();
-                
+
 
             }
 
@@ -227,10 +319,16 @@ namespace ShoppingCartApp
                     while (willAddAnother)
                     {
                         Console.WriteLine(Environment.NewLine);
-                        Console.WriteLine("Choose an option");
+                        Console.Write("Choose an option: ");
 
                         DisplayProducts(productsFromRepo);
-                        int itemChoice = int.Parse(Console.ReadLine());
+                        
+                        bool isInt = int.TryParse(Console.ReadLine(), out int itemChoice);
+                        while (isInt == false)
+                        {
+                            Console.Write("Choose an option: ");
+                            isInt = int.TryParse(Console.ReadLine(), out itemChoice);
+                        }
 
 
                         foreach (var product in productsFromRepo.Products)
@@ -242,7 +340,14 @@ namespace ShoppingCartApp
                                 while (tooManySelected)
                                 {
                                     Console.WriteLine("How many would you like?");
-                                    int quantity = Convert.ToInt16(Console.ReadLine());
+
+                                    isInt = int.TryParse(Console.ReadLine(), out int quantity);
+                                    while (isInt == false)
+                                    {
+                                        Console.WriteLine("How many would you like?");
+                                        isInt = int.TryParse(Console.ReadLine(), out quantity);
+                                    }
+
 
                                     if (quantity > product.Inventory)
                                     {
@@ -266,8 +371,15 @@ namespace ShoppingCartApp
 
                         Thread.Sleep(1000);
                         Console.Clear();
-                        Console.WriteLine("Would you like to add another item to your cart? (y)es or (n)o");
+                        Console.Write("Would you like to add another item to your cart? (y)es or (n)o: ");
                         string addAnotherItem = Console.ReadLine().ToLower().Trim();
+
+                        while (string.IsNullOrEmpty(addAnotherItem))
+                        {
+                            Console.Write("Would you like to add another item to your cart? (y)es or (n)o: ");
+                            addAnotherItem = Console.ReadLine().ToLower().Trim();
+                        }
+
 
                         if (addAnotherItem != "y")
                         {
@@ -278,8 +390,15 @@ namespace ShoppingCartApp
                     }
 
 
-                    Console.WriteLine("Are you ready to check out? (y)es or (n)o");
+                    Console.Write("Are you ready to check out? (y)es or (n)o: ");
                     readyToCheckout = Console.ReadLine().ToLower().Trim();
+
+                    while (string.IsNullOrEmpty(readyToCheckout))
+                    {
+                        Console.Write("Are you ready to check out? (y)es or (n)o: ");
+                        readyToCheckout = Console.ReadLine().ToLower().Trim();
+                    }
+
                     Console.Clear();
 
                 } while (readyToCheckout != "y");
@@ -303,7 +422,7 @@ namespace ShoppingCartApp
                 Console.WriteLine($"The new total cart price before tax is {totalCartPriceBeforeTax:C} {Environment.NewLine}");
                 Console.WriteLine($"The new total cart price after tax is {totalCartPriceAfterTax:C} {Environment.NewLine}");
                 Thread.Sleep(3000);
-                Console.Clear(); 
+                Console.Clear();
             }
 
 
@@ -321,16 +440,12 @@ namespace ShoppingCartApp
 
         private static void DisplayProducts(ProductRepo productRepo)
         {
-
-            int displayNumber = 1;
             Console.WriteLine("**********************************************************");
 
             //display the repo
             foreach (var item in productRepo.Products)
             {
-                Console.WriteLine($"({displayNumber}) {item.Name.ToUpper()} | {item.Description.ToUpper()} | {item.Category.ToUpper()} | {item.Price}");
-
-                displayNumber++;
+                Console.WriteLine($"({item.Id}) {item.Name.ToUpper()} | {item.Description.ToUpper()} | {item.Category.ToUpper()} | {item.Price}");
             }
             Console.WriteLine("**********************************************************");
             Console.WriteLine();
@@ -339,16 +454,12 @@ namespace ShoppingCartApp
 
         private static void DisplayProductsForAdmin(ProductRepo productRepo)
         {
-
-            int displayNumber = 1;
             Console.WriteLine("**********************************************************");
 
             //display the repo
             foreach (var item in productRepo.Products)
             {
-                Console.WriteLine($"({displayNumber}) {item.Name.ToUpper()} | {item.Description.ToUpper()} | {item.Category.ToUpper()} | {item.Price} | {item.Inventory}");
-
-                displayNumber++;
+                Console.WriteLine($"({item.Id}) {item.Name.ToUpper()} | {item.Description.ToUpper()} | {item.Category.ToUpper()} | {item.Price} | {item.Inventory}");
             }
             Console.WriteLine("**********************************************************");
             Console.WriteLine();
